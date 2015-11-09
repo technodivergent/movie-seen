749cc296ea851208b0855065fb30bc002794a326
George Hall <technikalmind@gmail.com>
2015-11-09 02:38:57 -0500

    Load scripts at bottom of page

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,5 @@
 </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="inc/js/bootstrap.min.js"></script>
 </body>
 </html>
--- inc/header.php
+++ inc/header.php
@@ -17,8 +17,6 @@ session_start();
   </head>
 
 <body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="inc/js/bootstrap.min.js"></script>
 
 <nav class="navbar navbar-inverse navbar-fixed-top">
 	<div class="container">
