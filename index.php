6c1862febfa44701c48b2988523b13462384a218
George Hall <technikalmind@gmail.com>
2015-11-09 03:04:43 -0500

    Removing unnecessary pages

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 include("inc/header.php");
-
-if(!isset($_GET['page']) || empty($_GET['page']) || $_GET['page'] == "home") {
-	include("pages/index.php");
-} elseif($_GET['page'] == "about") {
-	include("pages/about.php");
-} elseif($_GET['page'] == "contact") {
-	include("pages/contact.php");
-}
-
+include("pages/index.php");
 include("inc/footer.php");
 ?>
