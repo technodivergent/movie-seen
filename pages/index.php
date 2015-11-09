160333265ed6a8714b38d396ede9d108cf9c6677
George Hall <technikalmind@gmail.com>
2015-11-09 02:40:02 -0500

    Add text for welcome and about

--- pages/index.php
+++ pages/index.php
@@ -1,1 +1,28 @@
-<p>Welcome to Movie Seen! This project allows users to search for movies and display information about them. It also allows them to login/register to keep track of movies they have seen, or ones they would like to see.</p>
+<div class="col-sm-12">
+	<div class="panel panel-primary">
+		<div class="panel-heading">
+			<h3 class="panel-title">Welcome to Movie Seen</h3>
+		</div>
+		<div class="panel-body">
+			<div class="row">
+				<div class="col-sm-3">
+				<h4>What's Movie Seen?</h4>
+				<p>This project allows users to search for movies and display information about them. It also allows them to login/register to keep track of movies they have seen, or ones they would like to see.</p>
+				</div>
+				<div class="col-sm-3">
+				<h4>Why did you create this?</h4>
+				<p>To build something fun using the <a href="http://omdbapi.com">OMDb API</a>, learn something new, and practice programming</p>
+				</div>
+				<div class="col-sm-3">
+				<h4>What did you use or learn?</h4>
+				<p>Built with HTML/CSS, Bootstrap, PHP/MySQL. Topics learned include building CRUD functionality, database design/querying, processing third-party API, and JSON.</p>
+				</div>
+				<div class="col-sm-3">
+				<h4>What's next?</h4>
+				<p>Head on over to <a href="https://github.com/technikalmind/movie-seen/issues">GitHub</a> page to see my issue tracker. There are some additional features I'd like to implement, as well as improving the design of the database.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
