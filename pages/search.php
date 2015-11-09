be529f8076b467832fd410b171d2ac01e0cc846e
George Hall <technikalmind@gmail.com>
2015-11-09 01:46:12 -0500

    Update search layout

---
+++ pages/search.php
@@ -0,0 +1,13 @@
+<form class="form-horizontal" action="search.php" method="GET">
+				<div class="form-group">
+					<label for="search" class="control-label sr-only">Username</label>
+					<div class="col-sm-12">
+						<input type="text" name="s" id="search" placeholder="Search a movie or TV show..." class="form-control">
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-sm-4">
+						<button type="submit" class="btn btn-primary">Search</button>
+					</div>
+				</div>
+</form>
--- search.php
+++ search.php
@@ -2,32 +2,34 @@
 include("inc/header.php");
 include("inc/functions.php");
 ?>
-	<form action="search.php" method="GET">
-		<table>
-			<tr>
-				<td>Search:</td>
-				<td><input type="text" name="s" required="required"/><input type="submit" value="Search"/></td>
-			</tr>
-		</table>
-	</form>
+<div class="col-sm-7">
+	<div class="panel panel-info">
+		<div class="panel-heading">
+			<h3 class="panel-title">Search</h3>
+		</div>
+		<div class="panel-body">
+			<?php
+			if(empty($_GET['s']) || !isset($_GET['s'])) {
+				include("pages/search.php");
+			} elseif(isset($_GET['s'])) {
+				$term = rawurlencode($_GET["s"]);
+				$obj = fetchJSON("s", $term);
+				
+				if(isset($obj['Response'])) {
+					print ('Results not found!');
+				} else {
+					$search = $obj['Search'];
+					
+					foreach($search as $movie) {
+						print ('<a href="movie.php?id='.$movie['imdbID'].'"</a>'.$movie['Title']." (".$movie['Year'].')</a><br/>');
+					}
+					print('<a class="btn btn-success pull-right" role="button" href="search.php">New search</a>');
+				}
+			}
+			?>
+		</div>
+	</div>
+</div>
 <?php
 include("inc/footer.php");
-?>
-
-<?php
-if(empty($_GET['s'])) {
-} elseif(isset($_GET['s'])) {
-	$term = rawurlencode($_GET["s"]);
-	$obj = fetchJSON("s", $term);
-	
-	if(isset($obj['Response'])) {
-		print ('Results not found!');
-	} else {
-		$search = $obj['Search'];
-		
-		foreach($search as $movie) {
-			print ('<a href="movie.php?id='.$movie['imdbID'].'"</a>'.$movie['Title']." (".$movie['Year'].')</a><br/>');
-		}
-	}
-}
 ?>
