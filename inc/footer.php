aba41a2d07fde1661c3273e4233816a67e278c52
George Hall <technikalmind@gmail.com>
2015-11-09 02:39:44 -0500

    Add footer link

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,4 @@
+<footer>&copy; 2015 <a href="http://technikalmind.net">Technikal Mind</a></footer>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="inc/js/bootstrap.min.js"></script>
