68c82cf984e6047cf292dca3618d7a2daae2d039
George Hall <technikalmind@gmail.com>
2015-11-12 23:17:52 -0500

    Fix footer not showing up at bottom

--- pages/register.php
+++ pages/register.php
@@ -1,34 +1,36 @@
-<div class="col-sm-7">
-	<div class="panel panel-primary">
-		<div class="panel-heading">
-			<h3 class="panel-title">Register</h3>
-		</div>
-		<div class="panel-body">
-			<form class="form-horizontal" action="register.php" method="POST">
-				<div class="form-group">
-					<label for="username" class="col-sm-2 control-label">Username</label>
-					<div class="col-sm-4">
-						<input type="text" class="form-control" name="username" placeholder="Username">
+<div class="row">
+	<div class="col-sm-7">
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h3 class="panel-title">Register</h3>
+			</div>
+			<div class="panel-body">
+				<form class="form-horizontal" action="register.php" method="POST">
+					<div class="form-group">
+						<label for="username" class="col-sm-2 control-label">Username</label>
+						<div class="col-sm-4">
+							<input type="text" class="form-control" name="username" placeholder="Username">
+						</div>
 					</div>
-				</div>
-				<div class="form-group">
-					<label for="email" class="col-sm-2 control-label">Email</label>
-					<div class="col-sm-4">
-						<input type="email" class="form-control" name="email" placeholder="Email">
+					<div class="form-group">
+						<label for="email" class="col-sm-2 control-label">Email</label>
+						<div class="col-sm-4">
+							<input type="email" class="form-control" name="email" placeholder="Email">
+						</div>
 					</div>
-				</div>
-				<div class="form-group">
-					<label for="password" class="col-sm-2 control-label">Password</label>
-					<div class="col-sm-4">
-						<input type="password" class="form-control" name="password" placeholder="Password">
+					<div class="form-group">
+						<label for="password" class="col-sm-2 control-label">Password</label>
+						<div class="col-sm-4">
+							<input type="password" class="form-control" name="password" placeholder="Password">
+						</div>
 					</div>
-				</div>
-				<div class="form-group">
-					<div class="col-sm-offset-2 col-sm-4">
-						<button type="submit" class="btn btn-primary">Sign up</button>
+					<div class="form-group">
+						<div class="col-sm-offset-2 col-sm-4">
+							<button type="submit" class="btn btn-primary">Sign up</button>
+						</div>
 					</div>
-				</div>
-			</form>
+				</form>
+			</div>
 		</div>
 	</div>
 </div>
